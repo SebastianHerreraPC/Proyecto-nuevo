e3dd9deef2c7755de93c55db88d284d8b9a7373c
SebastianHerreraPc <sebehepe00@gmail.com>
2024-09-12 22:20:55 -0600

    card template

--- apps/app01.js
+++ apps/app01.js
@@ -1,26 +1,52 @@
-class PodcastEpisode {
-  constructor(title, artist, guest, duration) {
-    this._title = title;
-    this._artist = artist;
-    this.isLiked = false;
-    this.guest = guest;
-    this.duration = duration;
-  }
-  like() {
-    this.isLiked = !isLiked;
-  }
-  getEpisodeInfo() {
-    return ` ${this._artist}. "${this._title}"- ${
-      this.guest
-    } (${this.getFormattedDuration()} )`;
-  }
-  getFormattedDuration() {
-    const minutes = Math.floor(this.duration / 60); //el número total de minutos
-    const seconds = this.duration % 60; // el resto de la división por 60
-    return `${minutes}:${seconds > 9 ? seconds : "0" + seconds}`;
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const editButton = document.querySelector(".profile__edit-button");
+  const closeButton = document.querySelector(".popup__close-button");
+  const profileName = document.querySelector(".profile__name-text");
+  const inputName = document.querySelector(".popup__input-name");
+  const inputjob = document.querySelector(".popup__input-job");
+  const profileJob = document.querySelector(".profile__job");
+  const popupEdit = document.querySelector(".popup__edit");
 
-const podcast1 = new PodcastEpisode("hola", "Mundo");
+  function popupOpen() {
+    popupEdit.style.display = "block";
+  }
+  function popupClose() {
+    popupEdit.style.display = "none";
+  }
+  editButton.addEventListener("click", function () {
+    popupOpen();
+    inputName.value = profileName.textContent;
+    inputjob.value = profileJob.textContent;
+    inputjob.value = "";
+    inputName.value = "";
+  });
+  closeButton.addEventListener("click", function (evt) {
+    popupClose();
+  });
+  popupEdit.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputjob.value;
 
-console.log(PodcastEpisode);
+    popupClose();
+  });
+
+  //add images
+  const popupAdd = document.querySelector(".popup__add");
+  const addButton = document.querySelector(".profile__add-button");
+  const closeAddButton = document.querySelector(".popup__close-add-button");
+
+  function popupAddOpen() {
+    popupAdd.style.display = "block";
+  }
+  function popupAddClose() {
+    popupAdd.style.display = "none";
+  }
+
+  addButton.addEventListener("click", function () {
+    popupAddOpen();
+  });
+  closeAddButton.addEventListener("click", function () {
+    popupAddClose();
+  });
+});
